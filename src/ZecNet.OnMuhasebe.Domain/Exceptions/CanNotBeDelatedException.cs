0b924cff2b04879814b1935c1e296a1ccd609996
ZECYazilim <zekeriyabilisim3@gmail.com>
2022-08-14 18:58:52 +0300

    AppService Manager Added.

---
+++ src/ZecNet.OnMuhasebe.Domain/Exceptions/CanNotBeDelatedException.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp;
+
+namespace ZecNet.OnMuhasebe.Exceptions;
+public class CanNotBeDelatedException:BusinessException
+{
+    public CanNotBeDelatedException():base(OnMuhasebeDomainErrorCodes.CanNotBeDelated)
+    {
+
+    }
+}
---
+++ src/ZecNet.OnMuhasebe.Domain/Extensions/EntityAsyncExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using ZecNet.OnMuhasebe.Exceptions;
+
+namespace ZecNet.OnMuhasebe.Extensions;
+public static class EntityAsyncExtensions
+{
+    public static async Task CodeAnyAsync<TEntity>(this IReadOnlyRepository<TEntity> repository
+        , string code, Expression<Func<TEntity,bool>> predicate,bool check=true)
+        where TEntity : class,IEntity
+    {
+        if (check && await repository.AnyAsync(predicate))
+            throw new DuplicateCodeException(code);
+    }
+    public static async Task EntityAnyAsync(this IReadOnlyRepository<OzelKod> repository,
+        Guid? id,OzelKodTuru kodTuru,KartTuru kartTuru,bool check = true)
+    {
+        if(check&&id!=null)
+        {
+            var anyAsync = await repository.AnyAsync(x => x.Id == id && x.KodTuru == kodTuru && x.KartTuru == kartTuru);
+            if (!anyAsync)
+                throw new EntityNotFoundException(typeof(OzelKod), id);
+        }
+    }
+    public static async Task RelationtalEntityAnyAsync<TEntity>(this IReadOnlyRepository<TEntity> repository,
+        Expression<Func<TEntity,bool>> predicate)
+        where TEntity : class,IEntity
+    {
+        var anyAsync=await repository.AnyAsync(predicate);
+
+        if (anyAsync)
+            throw new CanNotBeDelatedException();
+    }
+}
